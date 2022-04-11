fe921348d3e38721002f33772464e430ff46521f
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-11 11:04:08 -0700

    more services

--- jacdac/main.ts
+++ jacdac/main.ts
@@ -7,6 +7,26 @@ namespace modules {
      */
     //% fixedInstance whenUsed block="envirobit temperature"
     export const envirobitTemperature = new TemperatureClient("envirobit temperature?device=self")
+    /**
+     * Envirobit humidity sensor
+     */
+    //% fixedInstance whenUsed block="envirobit humidity"
+    export const envirobitHumidity = new HumidityClient("envirobit humidity?device=self")
+    /**
+     * Envirobit pressure sensor
+     */
+    //% fixedInstance whenUsed block="envirobit pressure"
+    export const envirobitPressure = new AirPressureClient("envirobit pressure?device=self")
+    /**
+     * Envirobit sound level sensor
+     */
+    //% fixedInstance whenUsed block="envirobit sound level"
+    export const envirobitSoundLevel = new SoundLevelClient("envirobit sound level?device=self")
+    /**
+     * Envirobit color sensor
+     */
+    //% fixedInstance whenUsed block="envirobit color"
+    export const envirobitColor = new ColorClient("envirobit color?device=self")
 }
 
 namespace servers {
@@ -15,7 +35,27 @@ namespace servers {
             jacdac.createSimpleSensorServer(
                 jacdac.SRV_TEMPERATURE,
                 jacdac.TemperatureRegPack.Temperature,
-                () => envirobit.getTemperature()
+                () => envirobit.getTemperatureFine() / 100.0
+            ),
+            jacdac.createSimpleSensorServer(
+                jacdac.SRV_HUMIDITY,
+                jacdac.HumidityRegPack.Humidity,
+                () => envirobit.getHumidityFine() / 10000.0
+            ),
+            jacdac.createSimpleSensorServer(
+                jacdac.SRV_AIR_PRESSURE,
+                jacdac.AirPressureRegPack.Pressure,
+                () => envirobit.getPressureFine() / 100.0
+            ),
+            jacdac.createSimpleSensorServer(
+                jacdac.SRV_SOUND_LEVEL,
+                jacdac.SoundLevelRegPack.SoundLevel,
+                () => envirobit.getSoundLevel() / 443.0
+            ),
+            jacdac.createMultiSensorServer(
+                jacdac.SRV_COLOR,
+                jacdac.ColorRegPack.Color,
+                () => [envirobit.getRed() / 255.0, envirobit.getGreen() / 255.0, envirobit.getBlue() / 255.0]
             )
         ])
     }
--- jacdac/test.ts
+++ jacdac/test.ts
@@ -1,4 +1,7 @@
 forever(function() {
     pause(1000)
     console.logValue("temp", modules.envirobitTemperature.temperature())
+    console.logValue("humi", modules.envirobitHumidity.humidity())
+    console.logValue("pres", modules.envirobitPressure.pressure())
+    console.logValue("sound", modules.envirobitSoundLevel.soundLevel())
 })
