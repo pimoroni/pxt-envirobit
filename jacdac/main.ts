c2fbf0f9cbcf15b2f3c26b082a3bd8e403244bc1
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-26 07:08:41 -0700

    upgraded role queries

--- jacdac/main.ts
+++ jacdac/main.ts
@@ -6,27 +6,27 @@ namespace modules {
      * Envirobit temperature sensor
      */
     //% fixedInstance whenUsed block="envirobit temperature"
-    export const envirobitTemperature = new TemperatureClient("envirobit temperature?device=self")
+    export const envirobitTemperature = new TemperatureClient("envirobit temperature?dev=self")
     /**
      * Envirobit humidity sensor
      */
     //% fixedInstance whenUsed block="envirobit humidity"
-    export const envirobitHumidity = new HumidityClient("envirobit humidity?device=self")
+    export const envirobitHumidity = new HumidityClient("envirobit humidity?dev=self")
     /**
      * Envirobit pressure sensor
      */
     //% fixedInstance whenUsed block="envirobit pressure"
-    export const envirobitPressure = new AirPressureClient("envirobit pressure?device=self")
+    export const envirobitPressure = new AirPressureClient("envirobit pressure?dev=self")
     /**
      * Envirobit sound level sensor
      */
     //% fixedInstance whenUsed block="envirobit sound level"
-    export const envirobitSoundLevel = new SoundLevelClient("envirobit sound level?device=self")
+    export const envirobitSoundLevel = new SoundLevelClient("envirobit sound level?dev=self")
     /**
      * Envirobit color sensor
      */
     //% fixedInstance whenUsed block="envirobit color"
-    export const envirobitColor = new ColorClient("envirobit color?device=self")
+    export const envirobitColor = new ColorClient("envirobit color?dev=self")
 }
 
 namespace servers {
