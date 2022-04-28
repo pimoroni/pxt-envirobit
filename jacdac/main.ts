828d9f0fc9e9479a2f499f53a31fed3ba026822d
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-28 08:55:17 -0700

    added description

--- jacdac/main.ts
+++ jacdac/main.ts
@@ -50,23 +50,27 @@ namespace servers {
 
     function start() {
         jacdac.productIdentifier = 0x3da1c67d
+        jacdac.deviceDescription = "Pimoroni enviro:bit"
         jacdac.startSelfServers(() => {
-            return [
+            const servers = [
                 jacdac.createSimpleSensorServer(
                     jacdac.SRV_TEMPERATURE,
                     jacdac.TemperatureRegPack.Temperature,
-                    () => envirobit.getTemperatureFine() / 100.0
-                ),
+                    () => envirobit.getTemperatureFine() / 100.0, {
+                    statusCode: jacdac.SystemStatusCodes.Initializing
+                }),
                 jacdac.createSimpleSensorServer(
                     jacdac.SRV_HUMIDITY,
                     jacdac.HumidityRegPack.Humidity,
-                    () => envirobit.getHumidityFine() / 10000.0
-                ),
+                    () => envirobit.getHumidityFine() / 10000.0, {
+                    statusCode: jacdac.SystemStatusCodes.Initializing
+                }),
                 jacdac.createSimpleSensorServer(
                     jacdac.SRV_AIR_PRESSURE,
                     jacdac.AirPressureRegPack.Pressure,
-                    () => envirobit.getPressureFine() / 100.0
-                ),
+                    () => envirobit.getPressureFine() / 100.0, {
+                    statusCode: jacdac.SystemStatusCodes.Initializing
+                }),
                 new SoundLevelServer(),
                 jacdac.createMultiSensorServer(
                     jacdac.SRV_COLOR,
@@ -74,6 +78,17 @@ namespace servers {
                     () => [envirobit.getRed() / 255.0, envirobit.getGreen() / 255.0, envirobit.getBlue() / 255.0]
                 )
             ]
+
+            control.runInParallel(() => {
+                envirobit.getTemperatureFine()
+                envirobit.getHumidityFine()
+                envirobit.getPressureFine()
+                pause(400)
+                for (const server of servers)
+                    server.setStatusCode(jacdac.SystemStatusCodes.Ready)
+            })
+
+            return servers
         })
     }
     start()
